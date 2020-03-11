c2626b2a1846f7d3e6c623327481167a386cac75
andytael <andy.tael@yahoo.com>
2020-03-11 18:07:42 -0500

    Update env_vars.sh

--- env_vars.sh
+++ env_vars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set env variables
-export comp_name=AndyTael
-export db_pwd=Wing7TIG_rant
-export db_name=AndyTaelATP
+export comp_name=<Name of the compartment for the ADB>
+export db_pwd=<Strong password e.g. the ADMIN password for ADB>
+export db_name=<Name of the ADB>
 export display_name=${db_name}
 export wallet_file=${db_name}
 export wallet_pwd=${db_pwd}
-export TNS_ADMIN=${HOME}/network/admin
+export TNS_ADMIN={$HOME}/network/admin
 export conn_string=${db_name}_LOW
