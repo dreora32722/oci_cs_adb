5335e702adf61aa52fcce4e888c4c2bbadc889f7
andytael <andy.tael@yahoo.com>
2020-03-11 18:10:44 -0500

    Create get_conn_string.sh

---
+++ get_conn_string.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Get connection string
+db_conn=$(oci db autonomous-database list -c ${ocid_comp} --query "data [?\"db-name\"=='${db_name}'] | [0].\"connection-strings\".low" --raw-output)
