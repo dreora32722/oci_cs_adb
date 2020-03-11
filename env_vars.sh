89135f282ab313cf46d65b32b474304b486d745d
andytael <andy.tael@yahoo.com>
2020-03-11 15:15:10 -0500

    Create env_vars.sh
    envrionment variables script

---
+++ env_vars.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# Set env variables
+export comp_name=AndyTael
+export db_pwd=Wing7TIG_rant
+export db_name=AndyTaelATP
+export display_name=${db_name}
+export wallet_file=${db_name}
+export wallet_pwd=${db_pwd}
+export TNS_ADMIN=${HOME}/network/admin
+export conn_string=${db_name}_LOW
