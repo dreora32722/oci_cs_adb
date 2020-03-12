6de6db3a6249d388b1bfece5f6068b96b949b9f7
andytael <andy.tael@yahoo.com>
2020-03-12 09:19:31 -0500

    Update get_adb_ocid.sh

--- get_adb_ocid.sh
+++ get_adb_ocid.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -z "$ocid_comp" ] || [ -z "$db_name"]
+then
+  echo "Error: missing variable definitions"
+  exit 1
+fi
+
 # Get ADB OCID
 adb_id=$(oci db autonomous-database list -c ${ocid_comp} --query "data [?\"db-name\"=='${db_name}'] | [0].id" --raw-output)
