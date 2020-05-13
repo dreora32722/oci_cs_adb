88e24f81c0ae3a57d28257df00c7428f9430e470
dreora32722 <32728359+dreora32722@users.noreply.github.com>
2020-05-13 09:56:06 +0930

    new file gets ocid for bucket

---
+++ get_bucket.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$ocid_bucket" ]
+then
+  echo "Error: missing variable definitions"
+  exit 1
+fi
+
+# Get Bucket_OCID
+ocid_bucket=$(oci os bucket get --bucket-name ${bucket_name} )
+# xxxx--query "data [?\"name\"=='${comp_name}'] | [0].id" --raw-output)
+
+echo "Bucket OCID: " ${ocid_bucket}
+export ocid_bucket;
